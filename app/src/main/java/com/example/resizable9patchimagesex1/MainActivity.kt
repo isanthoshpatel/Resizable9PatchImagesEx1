a172b6c09951b2576af6c48f977a9f1f0c8c2339
isanthoshpatel <isanthoshpatel@gmail.com>
2020-05-13 21:39:56 +0530

    Initial commit

---
+++ app/src/main/java/com/example/resizable9patchimagesex1/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.resizable9patchimagesex1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        //paste any png filen from pc, into drawable
+        //right click on png->new->create 9 path file -> img.9.png
+        //open img.9.png -> adjust content show, streching
+        //set as backgroud to textview
+
+        //9 patch png for not to blur background on content grow
+
+
+    }
+}
